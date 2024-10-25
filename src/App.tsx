c6a479a5f7ed194a6bea1ee4258c7aee7306fafa
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2024-10-25 10:25:17 +0300

    Revert "update"
    This reverts commit 291e958428ac573e4dd0344847c2b0c899288b74.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Layout from "./Components/Layout/Layout";
 
 const HomePage = lazy(() => import("../src/pages/HomePage/HomePage"));
 
 export default function App(): JSX.Element {
   return (
     <Router>
-      <Layout>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        </Suspense>
-      </Layout>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
