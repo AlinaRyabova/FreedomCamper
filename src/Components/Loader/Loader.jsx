eef9cbec8f7f5e27d1fab02340ce54701ae9221c
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2024-10-26 18:24:51 +0300

    update

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -3,17 +3,14 @@ import css from "./Loader.module.css";
 
 export default function Loader() {
   return (
-    <div className={css.headerContainer}>
+    <div className={css.loaderContainer}>
       <Audio
         height="180"
         width="180"
         radius="9"
         color="#e44848"
         ariaLabel="loading"
-        wrapperStyle
-        wrapperClass
       />
-      ;
     </div>
   );
 }
